1875dbcc46c9d4bdccc08ffc01dfe81e8fd6dacb
jh-soares <78885065+jh-soares@users.noreply.github.com>
2021-03-04 21:07:20 -0300

    Add files via upload

---
+++ JogoVelha/src/jogovelha/Computador.java
@@ -0,0 +1,21 @@
+
+package jogovelha;
+
+public class Computador extends Jogador{
+    
+    public Computador(int jogador){
+        super(jogador);
+        System.out.println("Jogador 'Computador' criado!");
+        
+    }
+    
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
